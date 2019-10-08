37dcda6d9b43eb0f525de75d65de9fdaca605f4a
Abigail Tran <mt2915@rit.edu>
2019-10-07 22:21:39 -0400

    Added config files

--- src/Prototype.java
+++ src/Prototype.java
@@ -1,5 +1,6 @@
 public class Prototype {
 
-    // This shall be interesting
+    // Replace this with your PATH. Not sure how to do relative path in Java
+    private String GOOGLE_APPLICATION_CREDENTIALS= "/Users/abigail_tran/Documents/SeniorProject/GoogleAPI/GoogleAPI/recruitr-dc755b4479a3.json";
 
 }
